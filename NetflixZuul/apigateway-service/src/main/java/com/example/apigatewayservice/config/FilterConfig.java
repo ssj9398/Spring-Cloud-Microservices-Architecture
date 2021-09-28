5c560b319dc89e7f6c2ac7e0ee24e514be3f1284
alisyabob <sjs939883@gmail.com>
2021-09-28 23:39:51 +0900

    yml filters 추가

--- NetflixZuul/apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
+++ NetflixZuul/apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+
 public class FilterConfig {
-    @Bean
+
     public RouteLocator gatewatyoutes(RouteLocatorBuilder builder){
         return builder.routes()
                 .route(r -> r.path("/first-service/**")
