542a28d776e31df96a809714a6bd8f8f91b9af2e
alisyabob <sjs939883@gmail.com>
2021-11-11 17:47:43 +0900

    Catalogs Microservice - 기능 구현(entity, dto, vo)

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+    value('CATALOG-001' 'Berlin', 100, 1500);
+insert into catalog(product_id, product_name, stock, unit_price)
+    value('CATALOG-002' 'Tokyo', 110, 1000);
+insert into catalog(product_id, product_name, stock, unit_price)
+    value('CATALOG-003' 'Stockholm', 120, 2500);
