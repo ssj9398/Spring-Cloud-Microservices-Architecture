1f325a9b3c17c0ad16e4447dd4c58afba94bb16b
alisyabob <sjs939883@gmail.com>
2021-11-15 04:34:15 +0900

    Users Microservice - AuthenticationFilter 추가

--- Users Microservice/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ Users Microservice/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -5,14 +5,28 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        //http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("192.168.0.3")
+                .and()
+                .addFilter(getAuthenticationFilter());
+
 
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 }
