c089c27ea207cd5dfd289c077f5bf173afb8083e
alisyabob <sjs939883@gmail.com>
2021-11-16 22:35:37 +0900

    Users Microservice - Authorization 테스트

--- NetflixZuul, Spring Cloud Gateway/apigateway-service/src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
+++ NetflixZuul, Spring Cloud Gateway/apigateway-service/src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -19,6 +19,7 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
     Environment env;
 
     public AuthorizationHeaderFilter(Environment env){
+        super(Config.class);
         this.env = env;
     }
 
